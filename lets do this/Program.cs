e0947493ccc0b04d18fc44b407635d3631a4f799
S07C09 <S07C09@S07C09>
2024-05-15 10:34:04 +0330

    Add project files.

---
+++ lets do this/Program.cs
@@ -0,0 +1,25 @@
+﻿while(true){
+    Console.Write("Enter a string(one,two,three):");
+    string input = Console.ReadLine();
+    if(input == "exit")
+    {
+        break;
+    }
+    int output = 0;
+    switch (input)
+    {
+        case "one":
+            output = 1;
+            break;
+        case "two":
+            output = 2;
+            break;
+        case "three":
+            output = 3;
+            break;
+        default:
+            output = 0;
+            break;
+    }
+    Console.WriteLine(output);
+}
